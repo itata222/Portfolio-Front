e2a01b87c82725ee9e7429e3d1fc560ae668d3d9
itata222 <itamar.hagai1@gmail.com>
2024-09-02 10:52:13 +0300

    added CI/CD

--- src/components/home/Me.tsx
+++ src/components/home/Me.tsx
@@ -42,7 +42,7 @@ const Me = () => {
     <div className="me">
       <h1>
         <span ref={myMe1} className="meTitle">
-          Hi! I'm
+          Hi! I'mmm
         </span>
         <span ref={myMe2} className="meTitle">
           Itamar
